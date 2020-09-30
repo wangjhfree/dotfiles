d561f39215b4bdcdb03b9b2abeb7d16098560ed9
wangjhfree <wangjhfree@qq.com>
2020-09-30 16:07:43 +0800

    update

--- macdeploy.sh
+++ macdeploy.sh
@@ -8,54 +8,46 @@ brew upgrade
 brew cleanup
 
 # install brew
-brew install autojump
 brew install wget
+brew install autojump
+
+# clone dotfiles
+brew install git
+git clone https://github.com/wangjhfree/dotfiles.git
+mv dotfiles .dotfiles
 
 # install vim
-brew install vim
-# brew install vim ctags git astyle
-wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x
-ln -s .dotfiles/vimrc .vimrc
+wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x # brew install vim ctags git astyle
+ln -s ${HOME}/.dotfiles/vimrc .vimrc
 
-# cheat 2.3.1
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s ${HOME}/.dotfiles/zshrc .zshrc
+
+# cheat 4.1.0 => cheat 自动生成conf.yml => cheatsheets
 brew install cheat
-rm -rf .cheat
-ln -s ${HOME}/.dotfiles/cheat ${HOME}/.cheat
-ln /usr/local/bin/cheat-darwin-amd64 /usr/local/bin/cheat
+ln -s ${HOME}/.dotfiles/cheat ${HOME}/.config
 
-brew install git
-brew install python3
+# 开发工具
 brew install mysql@5.7
 brew install redis
 brew install nginx
+
+# pip package
+brew install python3
+ln -s ${HOME}/.dotfiles/pip ${HOME}/.pip
+pip install ipython
+pip install virtualenv
 
 # 解压缩工具
 brew install unrar
 brew install xz
 brew install gzip
 
-# git 地址
-# git clone https://github.com/wangjhfree/dotfiles.git
-mv dotfiles .dotfiles
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s .dotfiles/zshrc .zshrc
-
-
-
 # ln-s
 ln -s .dotfiles/aliases .aliases
 ln -s .dotfiles/exports .exports
 ln -s .dotfiles/gitconfig .gitconfig
-
-
-
-
-# pip package
-ln -s ${HOME}/.dotfiles/pip ${HOME}/.pip
-pip install ipython
-pip install virtualenv
 
 # nodejs
 wget -qO- https://raw.github.com/creationix/nvm/v0.4.0/install.sh | sh
